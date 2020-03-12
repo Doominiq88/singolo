f2929d5619077e18a59a0c766417012edc45ecfb
Ivan P <88talem@gmail.com>
2020-03-12 16:09:32 +0300

    add: slider

--- script.js
+++ script.js
@@ -4,3 +4,77 @@ MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('li>a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
+
+// slider
+
+(function() {
+    const sliderInit = function(slider) {
+
+        let sliders = slider.querySelectorAll('.slider > .phones');
+
+        //   console.log(sliders);
+
+        sliders[0].classList.add('activity');
+
+        let buttonNext = document.createElement('div'),
+            buttonPrev = document.createElement('div');
+
+        buttonNext.innerHTML = ">";
+        buttonPrev.innerHTML = "<";
+
+        buttonNext.classList.add('next');
+        buttonPrev.classList.add('prev');
+
+        //console.log(buttonNext, buttonPrev);
+
+        slider.appendChild(buttonNext);
+        slider.appendChild(buttonPrev);
+
+        buttonNext.addEventListener('click', function() {
+
+            //console.log('Click next');
+
+            let activeElem = slider.querySelector('div.activity');
+
+            if (activeElem != null) {
+                activeElem.classList.remove('activity');
+
+                let nextElem = activeElem.nextElementSibling.classList.add('activity');
+
+                if (nextElem == null) {
+                    activeElem.classList.remove('activity');
+                    // nextElem.classList.add('active');
+                    activeElem.previousElementSibling.classList.add('activity');
+
+                }
+            }
+        });
+
+        buttonPrev.addEventListener('click', function() {
+
+            let activeElem = slider.querySelector('div.activity');
+
+            if (activeElem != null) {
+                activeElem.classList.remove('activity');
+
+                let nextElem = activeElem.previousElementSibling.classList.add('activity');
+
+                if (nextElem == null) {
+                    activeElem.classList.remove('activity');
+                    activeElem.previousElementSibling.classList.add('activity');
+                }
+            }
+        });
+
+    };
+
+    this.slider = function(selector) {
+
+        let slider = document.querySelector(selector);
+        if (!slider) {
+            return false;
+        };
+        sliderInit(slider);
+    };
+}());
+slider('.slider');
